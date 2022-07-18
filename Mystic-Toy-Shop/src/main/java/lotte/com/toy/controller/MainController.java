12dcb9a6b3640cacf8de3a9de7be25eebd74d555
Jin <julietjin94@gmail.com>
2022-07-18 14:13:03 +0900

    init : project setting

--- Mystic-Toy-Shop/src/main/java/lotte/com/toy/controller/MainController.java
+++ Mystic-Toy-Shop/src/main/java/lotte/com/toy/controller/MainController.java
@@ -1,17 +1,33 @@
 package lotte.com.toy.controller;
 
+import lotte.com.toy.dto.AuthDto;
+import lotte.com.toy.service.AuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
     private Logger log = LoggerFactory.getLogger(MainController.class);
 
+    @Autowired
+    AuthService authService;
+
     @GetMapping("/main.do")
-    public String main(){
+    public String main() {
         log.info("MainController main()");
+
+        List<AuthDto> list = authService.authList();
+
+//        for (AuthDto dto : list) {
+//            System.out.println(dto.getAuth_role());
+//        }
+
         return "main";
     }
 
