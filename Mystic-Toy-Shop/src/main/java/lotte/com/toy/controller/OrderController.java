d2f4607aec030487e7f7a65ee459059ea0300f19
ksh940925 <ksh9409255@naver.com>
2022-07-19 15:03:54 +0900

    feat: 메인페이지의 주문내역 기능 구현(리뷰기능은 아직 안함)

---
+++ Mystic-Toy-Shop/src/main/java/lotte/com/toy/controller/OrderController.java
@@ -0,0 +1,75 @@
+package lotte.com.toy.controller;
+
+import lotte.com.toy.dto.OrderDateDto;
+import lotte.com.toy.dto.OrderDetailDto;
+import lotte.com.toy.dto.OrderGroupDto;
+import lotte.com.toy.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class OrderController {
+    @Autowired
+    OrderService orderService;
+
+    @RequestMapping(value = "orderlist.do", method = RequestMethod.GET)
+    public String findAllOrder(Model model, HttpServletRequest req){
+        /*
+        로그인 검증 코드 들어가야 함
+         */
+        int id = (Integer)req.getSession().getAttribute("userId");
+        List<Integer> orderGroupList = orderService.findOrderGroupList();
+        if(orderGroupList.isEmpty()){
+            System.out.println("데이터없음!");
+            return "orderempty";
+        }
+        List<List<OrderDetailDto>> list = new ArrayList<>();
+        for(Integer i : orderGroupList){
+            List<OrderDetailDto> orderList = orderService.orderFindAll(new OrderGroupDto(id,i));
+            list.add(orderList);
+        }
+        model.addAttribute("orderList",list);
+        model.addAttribute("orderGroupList",orderGroupList);
+        return "order";
+    }
+
+    @RequestMapping(value = "orderDetail.do", method = RequestMethod.GET)
+    public String findOrder(Model model, int orderId){
+        OrderDetailDto order = orderService.orderFind(orderId);
+        if(order == null){
+            System.out.println("주문없음!!!!");
+            return "order";
+        }
+        model.addAttribute("order",order);
+        return "orderdetail";
+    }
+
+    @RequestMapping(value = "orderDateList.do", method = RequestMethod.GET)
+    public String findDateOrderList(String startDate, String endDate,Model model,HttpServletRequest req){
+        int id = (Integer)req.getSession().getAttribute("userId");
+        Timestamp sDate = Timestamp.valueOf(startDate+" 00:00:00");
+        Timestamp eDate = Timestamp.valueOf(endDate+" 00:00:00");
+        OrderDateDto orderDate = new OrderDateDto(sDate,eDate,id);
+        List<Integer> orderDateGroup = orderService.findOrderGroupDateList(orderDate);
+        if(orderDateGroup.isEmpty()){
+            System.out.println("데이터없음!");
+            return "orderempty";
+        }
+        List<List<OrderDetailDto>> dateList = new ArrayList<>();
+        for(Integer i : orderDateGroup){
+            List<OrderDetailDto> orderList = orderService.orderFindAll(new OrderGroupDto(id,i));
+            dateList.add(orderList);
+        }
+        model.addAttribute("dateList",dateList);
+        model.addAttribute("orderDateGroup",orderDateGroup);
+        return "orderdate";
+    }
+}
---
+++ Mystic-Toy-Shop/src/main/java/lotte/com/toy/dto/OrderDetailDto.java
@@ -0,0 +1,29 @@
+package lotte.com.toy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class OrderDetailDto {
+    private int order_id;
+    private int order_group;
+    private int order_total_price;
+    private String order_address;
+    private String order_phone;
+    private String order_comment;
+    private Timestamp order_date;
+    private int order_quantity;
+    private int user_id;
+    private String user_name;
+    private String product_name;
+    private String product_info;
+    private String product_img;
+    private int product_cost;
+    private int product_category;
+    private char ship_status;
+}
