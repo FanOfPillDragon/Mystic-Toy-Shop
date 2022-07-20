8f8ca94b75970f3bbe3a7f9578eb9e81d20a6561
tlsdnwn55 <tlsdnwn55@naver.com>
2022-07-20 15:53:15 +0900

    feat. ReviewDto , QnADto 생성

---
+++ Mystic-Toy-Shop/src/main/java/lotte/com/toy/dto/ReviewDto.java
@@ -0,0 +1,22 @@
+package lotte.com.toy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class ReviewDto {
+
+    private int review_id;
+    private String review_content;
+    private String review_rate;
+    private int review_writer;
+    private Timestamp review_register_date;
+    private int ref_id;
+    private int is_deleted;
+    private int product_id;
+}
